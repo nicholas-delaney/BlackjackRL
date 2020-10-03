c57a869a8a3caf554776635a944725f1b25adbdd
nick <nicholasndelaney@gmail.com>
2020-10-03 20:52:12 -0230

    blackjack reinforcement learning v1

--- client/javascript/BlackjackRL.js
+++ client/javascript/BlackjackRL.js
@@ -1,41 +1,139 @@
 class BlackjackRL {
-    constructor(){
+    constructor() {
         this.p = [];
         this.q = [];
-        this.state = null;
-        this.dealerState = null;
+        this.state = {};
+        this.dealerState = {};
+        this.newRound = true;
         this.env = new Environment();
-        this.actions = ["stick", "hit"];
-        this.EPSILON = 0.1;
-        this.GAMMA = 0.5;
-        this.ALPHA = 0.6;
+        this.actions = [0, 1];
+        this.EPSILON = 0.5;
+        this.GAMMA = 0.2;
+        this.ALPHA = 0.3;
         this.init();
     }
 
     init() {
-        // startNewHand();
-
-
         // initialize state space with policy(p) and values(q)
         let pS;                               // Player's current sum
         let dC;                               // Dealer's current up card
         let sA;                               // possible actions ([0] = stick, [1] = hit)
         let a;                                // whether or not player has an ace ([0] = no ace, [1] = yes ace)
-        let count = 0;
-        for (pS = 0; pS <= 31; pS++) {     
+        for (pS = 1; pS <= 33; pS++) {
             this.q[pS] = [];
             this.p[pS] = [];
-            for (dC = 1; dC <= 10; dC++) {
+            for (dC = 2; dC <= 11; dC++) {
                 this.q[pS][dC] = [];
-                this.p[pS][dC] = [];   
+                this.p[pS][dC] = [];
                 for (sA = 0; sA <= 1; sA++) {
                     this.q[pS][dC][sA] = [];
                     this.p[pS][dC][sA] = [];
-                    for (a = 0; a <= 1; a++) {     
+                    for (a = 0; a <= 1; a++) {
                         this.q[pS][dC][sA][a] = 1;
                         this.p[pS][dC][sA][a] = 1 / 2;
-                        count++;
                     }
+                }
+            }
+        }
+        this.state = this.env.startNewRound();
+        for (let i = 0; i < 10000000; i++) {
+            this.qLearningIteration();
+        }
+    }
+
+    qLearningIteration() {
+        // Q-Learning steps
+        let action = this.getNextAction();
+        let nextState = this.getNextState(action);
+        let reward = this.env.getReward(nextState, action);
+        this.updateValue(nextState, action, reward);
+        this.updatePolicy();
+        this.state = nextState;
+        if (this.env.isRoundOver()) {
+            this.state = this.env.startNewRound();
+            this.env.setRoundOver(false);
+        }
+    }
+
+    // select the next action to take based on the policy at the current state
+    // returns that action
+    getNextAction() {
+        // always hit if player score is 11 or less
+        let pSum = this.state.pSum;
+        if (pSum <= 11) {
+            return 1;
+        }
+        // pick a random action 10% of the time
+        let randomNum = Math.floor(Math.random() * 10);
+        if (randomNum < (this.EPSILON * 10)) {
+            return this.actions[Math.floor(Math.random() * 2)];
+        }
+        // otherwise pick the 'greedy' action based on current policy
+        else {
+            let action = -1;
+            let softAce = (this.state.softAce > 0) ? 1 : 0;
+            let v = -999;
+            for (let i in this.actions) {
+                if (this.p[this.state.pSum][this.state.dealerCard][softAce][i] >= v) {
+                    v = this.p[this.state.pSum][this.state.dealerCard][softAce][i];
+                    action = i;
+                }
+            }
+            return Number(action);
+        }
+    }
+
+    // find next state based on action taken from current state
+    getNextState(action) {
+        let nextState = {};
+        // if player sticks, state stays the same
+        if (action === 0) {
+            nextState = this.state;
+        }
+        // if player hits, update state based on the new card
+        else if (action === 1) {
+            nextState = this.env.playerHit(this.state);
+        }
+        else {
+            console.log("big ol error");
+        }
+        return nextState;
+    }
+
+    updateValue(nextState, action, reward) {
+        let maxActionNextState = -999;
+        let aces = (nextState.softAce > 0) ? 1 : 0;
+        let sAces = (this.state.softAce > 0) ? 1 : 0;
+        for (let i in this.actions) {
+            if (this.q[nextState.pSum][nextState.dealerCard][aces][i] >= maxActionNextState) {
+                maxActionNextState = this.q[nextState.pSum][nextState.dealerCard][aces][i];
+            }
+        }
+        let q = this.q[this.state.pSum][this.state.dealerCard][aces][action];
+        this.q[this.state.pSum][this.state.dealerCard][sAces][action] = q + this.ALPHA * (reward + this.GAMMA * maxActionNextState - q);
+    }
+
+    updatePolicy() {
+        let sAces = (this.state.softAce > 0) ? 1 : 0;
+        let maxActionValue = -999;
+        let maxActions = [];
+        for (let i in this.actions) {
+            if (this.q[this.state.pSum][this.state.dealerCard][sAces][i] > maxActionValue) {
+                maxActionValue = this.q[this.state.pSum][this.state.dealerCard][sAces][i];
+                maxActions = [];
+                maxActions.push(Number(i));
+            }
+            else if (this.q[this.state.pSum][this.state.dealerCard][sAces][i] === maxActionValue) {
+                maxActions.push(Number(i));
+            }
+        }
+        for (let i in this.actions) {
+            for (let j in maxActions) {
+                if (maxActions[j] === this.actions[i]) {
+                    this.p[this.state.pSum][this.state.dealerCard][sAces][i] = 1 / maxActions.length;
+                }
+                else {
+                    this.p[this.state.pSum][this.state.dealerCard][sAces][i] = 0;
                 }
             }
         }
--- client/javascript/Environment.js
+++ client/javascript/Environment.js
@@ -1,5 +1,166 @@
 class Environment {
     constructor() {
-        
+        this.deck = [];
+        this.actions = [];
+        this.pHand = [];
+        this.dHand = [];
+        this.dTopCard = {};
+        this.roundOver = false;
+        this.init();
+    }
+    // initialize the environment
+    init() {
+        // initialize and shuffle the deck
+        for (let v = 1; v <= 13; v++) {
+            for (let r = 1; r <= 4; r++) {
+                this.deck.push({value: v, rank: r});
+            }
+        }
+        this.shuffleDeck();
+        // initialize actions: [0] = stick, [1] = hit
+        this.actions.push(0, 1);
+    }
+    // sets up the environment for a new round of blackjack
+    startNewRound() {
+        this.shuffleDeck();
+        this.deal2Cards();
+        // find sum of player's hand and how many aces they have
+        let aces = 0;
+        let sum = 0;
+        let cValue = 0;
+        for (let i in this.pHand) {
+            cValue = (this.pHand[i].value >= 10) ? 10 : this.pHand[i].value;
+            if (this.pHand[i].value === 1) {
+                cValue = 11;
+                aces++;
+            }
+            sum += cValue;
+        }
+        let dValue = (this.dHand[1].value >= 10) ? 10 : (this.dHand[1].value === 1) ? 11 : this.dHand[1].value;
+        let state = {
+            pSum: sum,
+            dealerCard: dValue,
+            softAce: aces,
+        }
+        return state;
+    }
+    // shuffle the deck using the Fisher-Yates shuffle method
+    shuffleDeck() {
+        // re-initialize the deck
+        this.deck = [];
+        for (let v = 1; v <= 13; v++) {
+            for (let r = 1; r <= 4; r++) {
+                this.deck.push({value: v, rank: r});
+            }
+        }
+        // do the shuffle 5 times
+        for (let n = 0; n < 5; n++) {
+            // Fisher-Yates shuffle method
+            for (let i = this.deck.length - 1; i > 0; i--) {
+                let j = Math.floor(Math.random() * i);
+                let k = this.deck[i];
+                this.deck[i] = this.deck[j];
+                this.deck[j] = k;
+            }   
+        }
+    }
+    // deal the player and the dealer 2 cards each to start the game
+    // check for 'natural win' : the player or the dealer has 21 on first 2 cards
+    deal2Cards() {
+        this.pHand = [];
+        this.dHand = [];
+        // deal the dealer and the player 2 cards 
+        for (let i = 0; i < 2; i++) {
+            this.pHand.push(this.deck.pop());
+            this.dHand.push(this.deck.pop());
+        }
+        // set the dealer top card
+        this.dTopCard = this.dHand[1];
+        // check for natural win
+    }
+    // deal one card (return the card from top of the deck)
+    dealerHit() {
+        return this.deck.pop();
+    }
+
+    playerHit(state) {
+        // update player hand with new card
+        let newCard = this.deck.pop();
+        this.pHand.push(newCard);
+        // update state with new card
+        let cValue = (newCard.value >= 10) ? 10 : (newCard.value === 1) ? 11 : newCard.value;
+        state.pSum += cValue;
+        state.softAce = (newCard.value === 1) ? state.softAce + 1 : state.softAce;
+        return state;
+    }
+
+    // get player's reward based on the nextState and the action taken from the previous state
+    getReward(nextState, action) {
+        // return -1 reward if the player busts, unless they have a usable ace
+        if (nextState.pSum > 21) {
+            if (nextState.softAce > 0) {
+                nextState.pSum -= 10;
+                nextState.softAce -= 1;
+            }
+            else {
+                this.setRoundOver(true);
+                return -1;
+            }
+        }
+        // if the player sticks (action = 0) or has 21 (blackjack), get reward based on dealer's policy
+        if (nextState.pSum === 21 || action === 0) {
+            this.setRoundOver(true);
+            return this.dealerPolicy(nextState.pSum);
+        }
+        // if the player hit return no reward (the round continues)
+        else if (action === 1)  {
+            return 1;
+        }
+    }
+
+    // dealer's turn: hit until sum is 17 or greater then check if player or dealer win
+    // playerSum : the players total card sum after they stick
+    dealerPolicy(playerSum) {
+        let dealerSum = 0;
+        let cardValue = 0;
+        let aces = 0;
+        // get the dealer's hand total sum and how many aces they have
+        for (let i in this.dHand) {
+            cardValue = (this.dHand[i].value > 10) ? 10 : (this.dHand[i].value === 1) ? 11 : this.dHand[i].value;
+            aces = (this.dHand[i].value === 1) ? aces + 1 : aces;
+            dealerSum += cardValue;
+        }
+        if (aces === 2) {
+            dealerSum = 12;
+            aces -= 1;
+        } 
+
+        let newCard = {};
+        let isBust = false;
+        let cValue = 0;
+        // dealer keeps getting cards until their sum is 17 or greater
+        while (dealerSum < 17) {
+            newCard = this.dealerHit();
+            this.dHand.push(newCard);
+            cValue = newCard.value >= 10 ? 10 :(newCard.value === 1) ? 11 : newCard.value;
+            dealerSum += cValue;
+            aces = (newCard.value === 1) ? aces + 1 : aces;
+            if (dealerSum > 21 && aces > 0) {
+                dealerSum -= 10;
+                aces -= 1;
+            }
+        }
+        // determine if win, lose or draw for the player (1 = win, 0 = draw, -1 = lose)
+        isBust = (dealerSum > 21) ? true : false;
+        let reward = (playerSum > dealerSum) ? 2 : (playerSum === dealerSum) ? 1 : (isBust) ? 2 : -2;
+        return reward;
+    }
+
+    setRoundOver(status) {
+        this.roundOver = status;
+    }
+
+    isRoundOver() {
+        return this.roundOver;
     }
 }
